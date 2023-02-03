d426730aeed7f62178882c648950715e96ab93c7
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-02-03 08:29:13 -0300

    fix: fixing erros codes and crashes

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -4,22 +4,38 @@ import { pollCollection, choiceCollection, voteCollection } from "../database/db
 export async function getResult(req, res) {
     const id = req.params.id
     const poll = await pollCollection.findOne({ _id: ObjectId(id) })
-    const choices = await choiceCollection.find({ pollId: poll._id.toString() }).toArray()
-    const votes = await getVotes(choices)
-    const winner = await getWinner(votes)
-    const numberWinnerVotes = countWinnerVotes(votes, winner)
-    const result = {
-        title: poll.title,
-        expireAt: poll.expireAt,
-        result: {
-            title: winner.title,
-            votes: numberWinnerVotes
+
+    if (poll === undefined || poll === null) {
+        res.status(404).send("Poll not found")
+    } else {
+
+        const choices = await choiceCollection.find({ pollId: poll._id.toString() }).toArray()
+        const votes = await getVotes(choices)
+        const winner = await getWinner(votes)
+        const numberWinnerVotes = countWinnerVotes(votes, winner)
+
+        const result = {
+            title: poll.title,
+            expireAt: poll.expireAt,
+            result: {
+                title: "",
+                votes: 0
+            }
         }
-    }
-    try {
-        res.status(201).send(result)
-    } catch (error) {
-        res.status(500).send("Houve um problema no servidor")
+
+        if (winner === undefined || winner === null) {
+            result.result.title = "Não teve votos"
+        } else {
+            result.result.title = winner.title,
+            result.result.votes = numberWinnerVotes
+        }
+
+        try {
+            res.status(201).send(result)
+        } catch (error) {
+            res.status(500).send("Houve um problema no servidor")
+        }
+
     }
 }
 
--- src/controllers/vote.controller.js
+++ src/controllers/vote.controller.js
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
 import { ObjectId } from "mongodb";
-import { voteCollection, choiceCollection, pollCollection} from "../database/db.js";
+import { voteCollection, choiceCollection, pollCollection } from "../database/db.js";
 
-export async function vote(req, res){
+export async function vote(req, res) {
     const id = req.params.id
 
     const vote = {
@@ -11,32 +11,32 @@ export async function vote(req, res){
     }
 
     const choice = await findChoice(vote.choiceId)
-    
-    if(choice === undefined || choice === null){
+
+    if (choice === undefined || choice === null) {
         res.status(404).send("Esta resposta não existe")
-    }
+    } else {
+        const poll = findPoll(choice.pollId)
+        const now = dayjs()
+        if (now.diff(poll.expireAt) < 0) {
+            res.status(403).send("Esta enquete já terminou")
+        }
 
-    const poll = findPoll(choice.pollId)
-    const now = dayjs()
-    if(now.diff(poll.expireAt) < 0){
-        res.status(403).send("Esta enquete já terminou")
-    }
+        try {
+            await voteCollection.insertOne(vote)
+            res.status(201).send("Voto registrado com sucesso")
+        } catch (error) {
 
-    try {
-        await voteCollection.insertOne(vote)
-        res.status(201).send("Voto registrado com sucesso")
-    } catch (error) {
-        
-        res.status(500).send("Houve um problema no servidor")
+            res.status(500).send("Houve um problema no servidor")
+        }
     }
 }
 
-async function findChoice(id){
+async function findChoice(id) {
     const choice = await choiceCollection.findOne({ _id: ObjectId(id) })
     return choice
 }
 
-async function findPoll(id){
+async function findPoll(id) {
     const poll = await pollCollection.findOne({ _id: ObjectId(id) })
     return poll
 }
